e3bf936ca2cb07a65b3af2d07b0ff655fac0874d
Joseph-marcel <zambio13@gmail.com>
2026-08-18 16:09:00 +0100

    initialize Application,add dependencies,configure yml,dev,docker files

---
+++ Taty-Touch-Fashion-Style/src/main/java/com/fashion/Taty/Touch/Fashion/Style/TatyTouchFashionStyleApplication.java
@@ -0,0 +1,13 @@
+package com.fashion.Taty.Touch.Fashion.Style;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TatyTouchFashionStyleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TatyTouchFashionStyleApplication.class, args);
+	}
+
+}
---
+++ Taty-Touch-Fashion-Style/src/test/java/com/fashion/Taty/Touch/Fashion/Style/TatyTouchFashionStyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fashion.Taty.Touch.Fashion.Style;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TatyTouchFashionStyleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
